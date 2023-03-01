ee685f9850521a000697a20ac23fe9f6aff0c037
zxcv2008a <zxcv2008a@gmail.com>
2023-03-01 12:49:58 +0900

    API finished

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 def create_app():
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    # Load the models
+    from todo.models import db
+    from todo.models.todo import Todo
 
-    from .views.routes import api
+    db.init_app(app)
+    # Create the database tables
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+    # Register the blueprints
+    from todo.views.routes import api
+
     app.register_blueprint(api)
 
     return app
--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, jsonify
- 
-api = Blueprint('api', __name__, url_prefix='/api/v1') 
+from flask import Blueprint, jsonify, request
+from todo.models import db
+from todo.models.todo import Todo
+from datetime import datetime
+
+api = Blueprint("api", __name__, url_prefix="/api/v1")
 
 TEST_ITEM = {
     "id": 1,
@@ -9,37 +13,67 @@ TEST_ITEM = {
     "completed": True,
     "deadline_at": "2023-02-27T00:00:00",
     "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
+    "updated_at": "2023-02-20T00:00:00",
 }
- 
-@api.route('/health') 
+
+
+@api.route("/health")
 def health():
     """Return a status of 'ok' if the server is running and listening to request"""
     return jsonify({"status": "ok"})
 
 
-@api.route('/todos', methods=['GET'])
+@api.route("/todos", methods=["GET"])
 def get_todos():
-    """Return the list of todo items"""
-    return jsonify([TEST_ITEM])
+    todos = Todo.query.all()
+    result = []
+    for todo in todos:
+        result.append(todo.to_dict())
+    return jsonify(result)
 
-@api.route('/todos/<int:todo_id>', methods=['GET'])
+
+@api.route("/todos/<int:todo_id>", methods=["GET"])
 def get_todo(todo_id):
-    """Return the details of a todo item"""
-    return jsonify(TEST_ITEM)
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({"error": "Todo not found"}), 404
+    return jsonify(todo.to_dict())
 
-@api.route('/todos', methods=['POST'])
+
+@api.route("/todos", methods=["POST"])
 def create_todo():
-    """Create a new todo item and return the created item"""
-    return jsonify(TEST_ITEM), 201
+    todo = Todo(
+        title=request.json.get("title"),
+        description=request.json.get("description"),
+        completed=request.json.get("completed", False),
+    )
+    if "deadline_at" in request.json:
+        todo.deadline_at = datetime.fromisoformat(request.json.get("deadline_at"))
+    # Adds a new record to the database or will update an existing record
+    db.session.add(todo)
+    # Commits the changes to the database, this must be called for the changes to be saved
+    db.session.commit()
+    return jsonify(todo.to_dict()), 201
 
-@api.route('/todos/<int:todo_id>', methods=['PUT'])
+
+@api.route("/todos/<int:todo_id>", methods=["PUT"])
 def update_todo(todo_id):
-    """Update a todo item and return the updated item"""
-    return jsonify(TEST_ITEM)
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({"error": "Todo not found"}), 404
+    todo.title = request.json.get("title", todo.title)
+    todo.description = request.json.get("description", todo.description)
+    todo.completed = request.json.get("completed", todo.completed)
+    todo.deadline_at = request.json.get("deadline_at", todo.deadline_at)
+    db.session.commit()
+    return jsonify(todo.to_dict())
 
-@api.route('/todos/<int:todo_id>', methods=['DELETE'])
+
+@api.route("/todos/<int:todo_id>", methods=["DELETE"])
 def delete_todo(todo_id):
-    """Delete a todo item and return the deleted item"""
-    return jsonify(TEST_ITEM)
- 
+    todo = Todo.query.get(todo_id)
+    if todo is None:
+        return jsonify({}), 200
+    db.session.delete(todo)
+    db.session.commit()
+    return jsonify(todo.to_dict()), 200
